ceb23bf23cd0a3a6e3b4de4fd13a2c9e71dcfdaf
Midori Fukami <midori@tfo.com.br>
2020-08-04 16:31:33 -0300

    Add pages video and category

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json'
-import BannerMain from './components/BannerMain'
-import Carousel from './components/Carousel'
-import Footer from './components/Footer'
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json'
+import BannerMain from '../../components/BannerMain'
+import Carousel from '../../components/Carousel'
+import Footer from '../../components/Footer'
 
-function App() {
+function Home() {
   return (
     <div style={{background: "#141414"}}>
       <Menu/>
@@ -13,7 +13,6 @@ function App() {
       <BannerMain 
       videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
       url={dadosIniciais.categorias[0].videos[0].url}
-      videoDescription={"O que é fron-end?"}
       />
 
       <Carousel
@@ -46,4 +45,4 @@ function App() {
   );
 }
 
-export default App;
+export default Home;
